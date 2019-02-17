29a389bbd305c6f775e9fb6df254d512d0fc8fe4
Michael Preston <michael.n.preston@gmail.com>
2019-02-17 05:29:17 -0600

    import all

--- src/images/index.js
+++ src/images/index.js
@@ -1,2 +1,11 @@
+//todo: maybe try: https://medium.com/@WebReflection/javascript-dynamic-import-export-b0e8775a59d4 (top of page)
+
 exports.blueberry = require('./blueberries/blueberry.jpg')
 exports.blackberry = require('./blackberries/container-of-blackberries.jpg')
+exports.gooseberry = require('./gooseberries/gooseberries.jpg');
+exports.cloudberry = require('./cloudberries/cloudberries.jpg');
+exports.acaiberry = require('./acaiberries/Acai-Berries.jpg')
+exports.salmonberry = require('./salmonberries/salmonberries.jpg');
+exports.red_mulberry = require('./red_mulberries/red_mulberries.jpg');
+exports.boysenberry = require('./boysenberries/organic-boysenberry.jpg');
+exports.raspberry = require('./raspberries/raspberries_closeup.jpg');
