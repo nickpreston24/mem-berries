14826f63d02d30cf4de0cfe9714bea4ee7bcfc40
Michael Preston <michael.n.preston@gmail.com>
2019-02-16 21:27:23 -0600

    Updates

---
+++ precache-manifest.7cbdaf663162441d138a40030674368c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6900f0ea2168e1e39f6e",
+    "url": "/mem-berries/static/js/runtime~main.6900f0ea.js"
+  },
+  {
+    "revision": "dc11cb15da8421f6fde5",
+    "url": "/mem-berries/static/js/main.dc11cb15.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/mem-berries/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "dc11cb15da8421f6fde5",
+    "url": "/mem-berries/static/css/main.bc2823af.chunk.css"
+  },
+  {
+    "revision": "718f7e3f1b3ba029bb65695cafc28d48",
+    "url": "/mem-berries/index.html"
+  }
+];
