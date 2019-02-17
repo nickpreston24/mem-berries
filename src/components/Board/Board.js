ef83b760225bfcd1652b13c876e874a1434a7e33
Michael Preston <michael.n.preston@gmail.com>
2019-02-17 06:46:14 -0600

    Added game reset; Need to fix smart rows

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,54 +1,63 @@
 import React, { Component } from 'react';
 import Card from '../Card/Card';
 import { BoardGenerator } from '../../BoardGenerator';
-const {shuffle} = require('../utils.js');
+import './style.css';
 
+const { shuffle } = require('../utils.js');
 const MAX_COLUMNS = 3;
 
 export class Board extends Component {
 
     constructor(props) {        
-        super(props);
-        let generator = props.generator || new BoardGenerator();
+        super(props);                
+        this.generator = props.generator || new BoardGenerator();        
         this.state = {
-            generator,
             score: 0,
-            cards: generator.makeBoard()
+            topScore: 0,
+            cards: this.generator.makeBoard()
         };
+
+        shuffle(this.state.cards);
+    }
+
+    reset() {
+        console.log('resetting game!');
+        this.setState({score:0, cards: this.generator.makeBoard()})
+        shuffle(this.state.cards);
     }
 
     onClick(index) {        
         
         let current = this.state.cards[index];
-        let {points, image} = current;       
+        let {points} = current;       
         
         if(current.visited)
-        {
-            // alert('GAME OVER');
-            this.setState({score : 0})
+        {      
+            let newScore = this.state.score;
+            if(newScore > this.state.topScore || 0)
+                this.setState({topScore: newScore});
+            this.setState({score: 0});
+            this.reset();
+            return;
         }
 
         current.visited = true;
         
         this.setState(state=>
         {
-            const list = state.cards;                       
-            list[index] = current;            
+            const list = state.cards;
+            state.score = state.score + points;
             return {list}
         });
-            
-        this.setState({
-            score: this.state.score + points,
-            cards: shuffle(this.state.cards)
-        })
-            
-        console.log('updated state: ', this.state.cards);        
-        console.log('total visited: ', this.state.cards.filter(c=>c.visited).length)
+        
+        shuffle(this.state.cards);
+        
+        let visited = this.state.cards.filter(c=>c.visited).length;
+        console.log('total visited: ', visited)
     }
 
     renderSquare(index, card) {
         let image = card.image;
-        console.log('i:', index)
         return <Card 
             image={image} 
             value={index} 
@@ -56,14 +65,11 @@ export class Board extends Component {
             onClick={()=>this.onClick(index)}>
         </Card>;
     }
-
+    
     render() {
-        
-        const status = `Current score: ${this.state.score}`;
-        
         return (
         <div>
-            <div className="status">{status}</div>
+            <h2 className="status">{`Current Score: ${this.state.score}\n Top Score: ${this.state.topScore}`}</h2>
             {this.arrangeTiles()}
         </div>);
     }
@@ -72,13 +78,14 @@ export class Board extends Component {
         const ROWS = Math.floor(this.state.cards.length / MAX_COLUMNS); //todo: figure out a smart way to get the number of rows w/o going over, i.e. 8 means 3 rows just like 9.
         console.log('max rows allowed: ', ROWS);
         return this.state.cards.map((card, index) => {
-            let makeNewRow = index % ROWS + 1;
-            console.info('make new row?', makeNewRow);
-            return (makeNewRow === ROWS) ?
-                <div key={index} className="board-row">
-                    {this.renderSquare(index, card)}
-                </div>
-                : this.renderSquare(index, card);
+            return this.renderSquare(index, card);
+            // let makeNewRow = index % ROWS + 1;
+            // console.info('make new row?', makeNewRow);
+            // return (index % ROWS == 0) ?
+            //     <div key={index} className="board-row">
+            //         {this.renderSquare(index, card)}
+            //     </div>
+            //     : this.renderSquare(index, card);
         });
     }
 }
