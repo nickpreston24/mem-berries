deaf29a1861d6ec571f7af92f969acad8765f87f
Michael Preston <michael.n.preston@gmail.com>
2019-02-17 06:45:26 -0600

    generator passed in named props

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import { Board } from './components/Board/Board';
 import { BoardGenerator } from './BoardGenerator';
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Click the Berries!</h1>
-        <Board >{new BoardGenerator()}</Board>
+        <Board generator={new BoardGenerator()}></Board>
       </div>
     );
   }
