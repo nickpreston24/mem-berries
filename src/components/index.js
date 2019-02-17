75ef9b6bd02bc1234b22dd0fe39eee5016c2d66a
Michael Preston <michael.n.preston@gmail.com>
2019-02-16 23:32:31 -0600

    updated Card

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Card from './Card/Card';
+import Grid from './Grid/Grid';
+
+//TODO: bundle these up and export them if possible.
+module.exports = {
+    Grid,
+    Card
+}
+
+// export {Grid}
