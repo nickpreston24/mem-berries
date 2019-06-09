3e64644147789d073afbe1fa36e0e425910a6372
Michael Preston <michael.n.preston@gmail.com>
2019-06-08 20:46:42 -0500

    Updates

---
+++ precache-manifest.75cbd1043d2a5c64573ace3a0e04d451.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199b6572fcef7f3e59642669501cd91b",
+    "url": "/mem-berries/index.html"
+  },
+  {
+    "revision": "d1817ca2d1be62289ba6",
+    "url": "/mem-berries/static/css/main.f7cc547d.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/mem-berries/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "d1817ca2d1be62289ba6",
+    "url": "/mem-berries/static/js/main.e7e5bc17.chunk.js"
+  },
+  {
+    "revision": "e68b1c520bfd5bff36e7",
+    "url": "/mem-berries/static/js/runtime~main.30a570ee.js"
+  },
+  {
+    "revision": "d76e44e45f99b624075bd744c84a1e0d",
+    "url": "/mem-berries/static/media/Acai-Berries.d76e44e4.jpg"
+  },
+  {
+    "revision": "40904b070b61ef3585d5cf01827b69d7",
+    "url": "/mem-berries/static/media/blueberry.40904b07.jpg"
+  },
+  {
+    "revision": "2789986df6f5a44e8edfd1af9ff8ccd4",
+    "url": "/mem-berries/static/media/cloudberries.2789986d.jpg"
+  },
+  {
+    "revision": "ab56d77bb7dac07f28b819f9f4df58eb",
+    "url": "/mem-berries/static/media/container-of-blackberries.ab56d77b.jpg"
+  },
+  {
+    "revision": "c52f889d8ca28a268cf3b960ac096df7",
+    "url": "/mem-berries/static/media/gooseberries.c52f889d.jpg"
+  },
+  {
+    "revision": "a1605fea7a8209f42c4f0d0c61f0d321",
+    "url": "/mem-berries/static/media/organic-boysenberry.a1605fea.jpg"
+  },
+  {
+    "revision": "55ad5ac9e691e20153cda1c10dcec70a",
+    "url": "/mem-berries/static/media/raspberries_closeup.55ad5ac9.jpg"
+  },
+  {
+    "revision": "28d5666f60dd5a89051d49a70f40e827",
+    "url": "/mem-berries/static/media/red_mulberries.28d5666f.jpg"
+  },
+  {
+    "revision": "7c7880b0c082e96112a5d62974f21980",
+    "url": "/mem-berries/static/media/salmonberries.7c7880b0.jpg"
+  }
+]);
